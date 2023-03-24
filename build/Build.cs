7ee3c1e4cdda5ce214a3df1d77c01d4e8a8a1c65
Thomas Woodcock <thomas@thomaswoodcock.net>
2023-03-24 14:23:19 +0000

    Adds initial NUKE build targets

--- build/Build.cs
+++ build/Build.cs
@@ -1,26 +1,52 @@
 using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
     Target Clean => _ => _
-        .Before(Restore)
+        .Description("Cleans the project")
         .Executes(() =>
         {
+            DotNetClean(settings => settings.SetProject(Solution));
         });
 
     Target Restore => _ => _
+        .Description("Restores project dependencies")
+        .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(settings => settings.SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
+        .Description("Builds the project")
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(settings => settings
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
 
-    public static int Main() => Execute<Build>(x => x.Compile);
+    Target Test => _ => _
+        .Description("Tests the project")
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(settings => settings
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+    public static int Main() => Execute<Build>(x => x.Test);
 }
