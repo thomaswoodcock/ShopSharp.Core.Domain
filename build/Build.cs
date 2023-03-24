ad9d7dc345143454ad0b29182ccd26c904fcb693
Thomas Woodcock <thomas@thomaswoodcock.net>
2023-03-24 13:43:27 +0000

    Adds empty NUKE build project

---
+++ build/Build.cs
@@ -0,0 +1,26 @@
+using Nuke.Common;
+
+class Build : NukeBuild
+{
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+        });
+
+    public static int Main() => Execute<Build>(x => x.Compile);
+}
