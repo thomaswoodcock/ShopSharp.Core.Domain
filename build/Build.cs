416f826bb89bf05ee25e59a0ca7e98a7d9085f7c
Thomas Woodcock <thomas@thomaswoodcock.net>
2023-03-25 08:07:38 +0000

    Packs NuGet package in NUKE build

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,10 @@
 using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
+using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [GitHubActions(
@@ -19,10 +21,14 @@ class Build : NukeBuild
 
     [Solution(GenerateProjects = true)] readonly Solution Solution;
 
+    static AbsolutePath ArtifactDirectory => RootDirectory / "artifacts";
+    static AbsolutePath PackageDirectory => ArtifactDirectory / "packages";
+
     Target Clean => _ => _
-        .Description("Cleans the project")
+        .Description("Cleans the project and build artifact directory")
         .Executes(() =>
         {
+            EnsureCleanDirectory(ArtifactDirectory);
             DotNetClean(settings => settings.SetProject(Solution));
         });
 
@@ -60,5 +66,20 @@ class Build : NukeBuild
                 .EnableNoBuild());
         });
 
-    public static int Main() => Execute<Build>(x => x.Test);
+    Target Pack => _ => _
+        .Description("Generates the project package")
+        .DependsOn(Test)
+        .Produces(PackageDirectory)
+        .Executes(() =>
+        {
+            DotNetPack(settings => settings
+                .SetProject(Solution.ShopSharp_Core_Domain)
+                .SetConfiguration(Configuration)
+                .SetOutputDirectory(PackageDirectory)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+    public static int Main() => Execute<Build>(x => x.Pack);
 }
