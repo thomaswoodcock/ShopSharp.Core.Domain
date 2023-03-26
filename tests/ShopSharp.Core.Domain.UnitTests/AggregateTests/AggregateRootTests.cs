02fe9c5ab94d720d327e48a42ca9559304eb8a80
Thomas Woodcock <thomas@thomaswoodcock.net>
2023-03-26 19:35:00 +0100

    Adds AggregateRoot abstract class

---
+++ tests/ShopSharp.Core.Domain.UnitTests/AggregateTests/AggregateRootTests.cs
@@ -0,0 +1,53 @@
+﻿using ShopSharp.Core.Domain.AggregateTests.Fakes;
+
+namespace ShopSharp.Core.Domain.AggregateTests;
+
+public class AggregateRootTests
+{
+    private readonly FakeAggregateRoot _aggregateRoot = new();
+
+    [Theory]
+    [AutoData]
+    public void AddAndApplyDomainEventAppliesDomainEvent(FakeDomainEvent domainEvent)
+    {
+        // Arrange Act
+        _aggregateRoot.AddAndApplyFakeDomainEvent(domainEvent);
+
+        // Assert
+        _aggregateRoot.Value
+            .Should()
+            .Be(domainEvent.Value);
+    }
+
+    [Theory]
+    [AutoData]
+    public void AddAndApplyDomainEventAddsDomainEventToUncommittedDomainEvents(FakeDomainEvent domainEvent)
+    {
+        // Arrange Act
+        _aggregateRoot.AddAndApplyFakeDomainEvent(domainEvent);
+
+        // Assert
+        _aggregateRoot.UncommittedDomainEvents
+            .Should()
+            .ContainSingle()
+            .Which
+            .Should()
+            .Be(domainEvent);
+    }
+
+    [Theory]
+    [AutoData]
+    public void MarkDomainEventsAsCommittedClearsUncommittedDomainEvents(FakeDomainEvent domainEvent)
+    {
+        // Arrange
+        _aggregateRoot.AddAndApplyFakeDomainEvent(domainEvent);
+
+        // Act
+        _aggregateRoot.MarkDomainEventsAsCommitted();
+
+        // Assert
+        _aggregateRoot.UncommittedDomainEvents
+            .Should()
+            .BeEmpty();
+    }
+}
